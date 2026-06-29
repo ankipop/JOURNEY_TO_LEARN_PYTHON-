853dce1ebc4c4915bc1e4f9e7334e3b92809ab46
Daylight <ankipop8@gmail.com>
2026-06-29 09:57:50 +0530

    Add traffic light rules program

---
+++ concepts.py
@@ -0,0 +1,13 @@
+#CONDITIONAL STATEMENTS 
+#IF/ELIF/ELSE
+#WRITE A PROGRAM FOR TRAFFIC LIGHT RULES
+
+l=input("type the colour: ")
+if(l=="red"):
+    print("stop")
+elif(l=="green"):
+    print("go")
+elif(l=="yellow"):
+    print("look and wait")
+else:
+    print("wrong light")
