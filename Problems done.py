846890ee54037910a41c96cd1223ec5712b10ced
Daylight <ankipop8@gmail.com>
2026-06-29 09:58:35 +0530

    Add traffic light rules program to Problems done.py

--- Problems done.py
+++ Problems done.py
@@ -4,3 +4,15 @@ a=int(input("type 1st number: "))
 b=int(input("type 2nd number: "))
 sum=a+b
 print("sum is:",sum)
+
+
+#2.WRITE A PROGRAM FOR TRAFFIC LIGHT RULES
+l=input("type the colour: ")
+if(l=="red"):
+    print("stop")
+elif(l=="green"):
+    print("go")
+elif(l=="yellow"):
+    print("look and wait")
+else:
+    print("wrong light")
