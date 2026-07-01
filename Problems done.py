5186d683fd1d918e989c40c056557def024484d4
Daylight <ankipop8@gmail.com>
2026-07-01 19:51:49 +0530

    Implement Armstrong number check
    Added functionality to check if a number is an Armstrong number.

--- Problems done.py
+++ Problems done.py
@@ -46,3 +46,22 @@ elif age > 13 and age <= 18:
     print("teenager")
 else:
     print("adult")
+    
+#6.CHECK IF AN ARMSTRONG NUMBER IS TYPED BY USER
+n = int(input("Type your number: "))
+digits = 0
+k=l=n
+while l != 0:
+    digits += 1
+    l = l // 10
+print("no of digits is ",digits)
+sumOFpowers=0
+while k != 0:
+    r=k%10
+    sumOFpowers+=r**digits
+    k=k//10
+if n== sumOFpowers:
+    print("yes! armstrong number")
+else:
+    print("no!not a armstrong number")
+    
